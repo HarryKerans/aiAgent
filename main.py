a25c2b35ce39fe3f07d0a62aa05809f8eb9792e4
HarryKerans <harry.kerans2@nhs.net>
2026-04-26 12:30:38 +0100

    Add ability to add user prompt

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-def main():
-    import os
-    from dotenv import load_dotenv
+from html import parser
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
-    load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    if not api_key:
-        raise RuntimeError("GEMINI_API_KEY not found in environment variables")
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if not api_key:
+    raise RuntimeError("GEMINI_API_KEY not found in environment variables")
 
-    from google import genai
 
-    client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
-    print(response.text)
-if __name__ == "__main__":
-    main()
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=messages
+)
+if response.usage_metadata != None:
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+print(response.text)
