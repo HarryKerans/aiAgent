2193ff80a5ef4efef2de18e78af17b551ff8333e
HarryKerans <harry.kerans2@nhs.net>
2026-04-26 16:48:21 +0100

    first function

---
+++ functions/get_files_info.py
@@ -0,0 +1,15 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    if not os.path.isdir(working_directory):
+        print(f'Error: Working directory "{working_directory}" does not exist or is not a directory')
+        return
+    absolute_path = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(absolute_path, directory))
+    print(f"Target directory: {target_dir}")
+    # Will be True or False
+    valid_target_dir = os.path.commonpath([absolute_path, target_dir]) == absolute_path
+    if not valid_target_dir:
+        print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+if __name__ == "__main__":
+    get_files_info("calculator", ".")
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ if not api_key:
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
@@ -22,7 +23,10 @@ response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents=messages
 )
-if response.usage_metadata != None:
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    if response.usage_metadata != None:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 print(response.text)
