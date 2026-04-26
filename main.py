cce8625ed0420ab645c58dd2ca16f5371b398cab
HarryKerans <harry.kerans2@nhs.net>
2026-04-26 10:44:30 +0100

    base hardcoded query

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 def main():
-    print("Hello from aiagent!")
+    import os
+    from dotenv import load_dotenv
 
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY not found in environment variables")
 
+    from google import genai
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print(response.text)
 if __name__ == "__main__":
     main()
